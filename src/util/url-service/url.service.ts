9899e7abf890a94c83432ac360a3e7c4a290b19b
FabioClem <fabio.henrique.clem@gmail.com>
2017-08-29 16:13:00 -0300

    Ajustes nos componentes, modificado urlIP da UrlService para dprcuradm0111:8080/ *Koalization*

--- src/util/url-service/url.service.ts
+++ src/util/url-service/url.service.ts
@@ -5,8 +5,9 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class UrlService {
 
-    private urlIp = "10.40.195.81:8080/";
-    //private urlIp = "10.200.35.67:80";
+    //private urlIp = "10.40.195.81:8080/"; // QA PeleServ
+    //private urlIp = "10.200.35.67:80/"; Produção CO
+    private urlIp = "dprcuradm0111:8080/"; // CREMO
 
     private headersAppJson = new Headers({ 'Content-Type': 'application/json' });
     public options = new RequestOptions({ headers: this.headersAppJson });
@@ -19,13 +20,13 @@ export class UrlService {
         this.hOtherUrl(otherUrl);
         switch (rqst) {
             case "get":
-                return this.httpGetRequest(command, _data, otherUrl);
+                return this.httpGetRequest(command, _data);
             case "post":
-                return this.httpPostRequest(command, _data, otherUrl);
+                return this.httpPostRequest(command, _data);
         }
     }
 
-    private httpPostRequest(command: string, _data: any, otherUrl?: string) {
+    private httpPostRequest(command: string, _data: any) {
         const url = `${this.url}` + command;
         return this.http.post(url, JSON.stringify(_data), this.options)
             .timeout(120000)
@@ -36,7 +37,7 @@ export class UrlService {
             .catch(this.handleError);
     }
 
-    private httpGetRequest(command: string, _data?: any, otherUrl?: string) {
+    private httpGetRequest(command: string, _data?: any) {
         let rstlink;
         if (_data) {
             rstlink = command + _data;
@@ -53,14 +54,13 @@ export class UrlService {
             .catch(this.handleError);
     }
 
-    hOtherUrl(l) {
+    private hOtherUrl(l) {
         if (l) {
-            this.urlIp = l;
+            this.url = "http://" + l;
         }
     }
 
     public handleError(error: any): Promise<any> {
-        //console.error('Ocorreu o seguinte erro: ', error); // for demo purposes only
         let er: any;
         if (error.message === "Timeout has occurred") {
             er = {
