c667261cefcaf60c5f169e6fb97f4d13d97b9910
FabioClem <fabio.henrique.clem@gmail.com>
2017-09-14 18:04:17 -0300

    Ajustes *Koalization*

--- src/co/configuracao-linha/mock/mock-sidenav-co.ts
+++ src/co/configuracao-linha/mock/mock-sidenav-co.ts
@@ -23,17 +23,17 @@ export const sideNavConfLinha: SideNav[] = [
     {
         nome: "Serviço",
         component: "servico-linha-component"
-    },
-    {
-        nome: "Listar Status das Portas",
-        component: "status-porta-component"
-    },
+    },    
     {
         nome: "Manobrar",
         component: "manobrar-linha-component"
     },
     {
-        nome: "Listar Status da Linha",
-        component: "status-linha-component"
+        nome: "Listar Status das Portas",
+        component: "status-porta-component"
     }
+    // {
+    //     nome: "Listar Status da Linha",
+    //     component: "status-linha-component"
+    // }
 ]
