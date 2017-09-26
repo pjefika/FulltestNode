c418d2081cac3b697ffae8f37fe919fd2fc698dc
FabioClem <fabio.henrique.clem@gmail.com>
2017-09-26 18:44:59 -0300

    fixes *Koalization*

--- src/util/comp_complementares/configuracoesporta/metalico/configuracoes-porta-metalico.component.ts
+++ src/util/comp_complementares/configuracoesporta/metalico/configuracoes-porta-metalico.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'configuracoes-porta-metalico-component',
-    templateUrl: 'configuracoes-porta-metalico.component.html'
+    templateUrl: 'configuracoes-porta-metalico.component.html',
+    styleUrls: ['configuracoes-porta-metalico.component.css']
 })
 
 export class ConfiguracoesPortaMetalicoComponent implements OnInit {
