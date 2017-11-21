f53e8d4fb9dac02dc73480a11047222849c4cb47
FabioClem <fabio.henrique.clem@gmail.com>
2017-11-21 14:51:25 -0200

    adicionado titulo para as pagina de manobra e fultlest, removido codigos comentados. *Koalization*

--- src/util/url-service/url.service.ts
+++ src/util/url-service/url.service.ts
@@ -62,7 +62,7 @@ export class UrlService extends SuperService implements RequestAction {
             .then(response => {
                 return response.json()
             })
-            .catch(super.handleError);
+            .catch(super.handleErrorKing);
     }
 
     public get(infoResquest: InfoRequest) {
@@ -79,24 +79,6 @@ export class UrlService extends SuperService implements RequestAction {
             .then(response => {
                 return response.json()
             })
-            .catch(super.handleError);
+            .catch(super.handleErrorKing);
     }
-
-    // public handleError(error: any): Promise<any> {
-    //     let er: any;
-    //     if (error.message === "Timeout has occurred") {
-    //         er = {
-    //             tError: "Timeout",
-    //             mError: "Tempo de busca excedido, por favor realize a busca novamente, caso o problema persista informe ao administrador do sistema."
-    //         }
-    //     } else {
-    //         let erJson: any;
-    //         erJson = error.json();
-    //         er = {
-    //             tError: "",
-    //             mError: erJson.message
-    //         }
-    //     }
-    //     return Promise.reject(er);
-    // }
 }
