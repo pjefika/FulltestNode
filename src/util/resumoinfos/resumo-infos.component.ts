bf54dc68350a09f91ae01da9401cf03e333562cb
FabioClem <fabio.henrique.clem@gmail.com>
2017-11-23 10:27:16 -0200

    adicionado animate in

--- src/util/resumoinfos/resumo-infos.component.ts
+++ src/util/resumoinfos/resumo-infos.component.ts
@@ -1,11 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { Cadastro } from 'viewmodel/cadastro/cadastro';
 import { HolderService } from 'util/holder/holder.service';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
     selector: 'resumo-infos-component',
     templateUrl: 'resumo-infos.component.html',
-    styleUrls: ['resumo-infos.component.css']
+    styleUrls: ['resumo-infos.component.css'],
+    animations: [
+        trigger('flyInOut', [
+            state('in', style({ transform: 'translateX(0)' })),
+            transition('void => *', [
+                style({ transform: 'translateX(-100%)' }),
+                animate(100)
+            ]),
+            transition('* => void', [
+                animate(100, style({ transform: 'translateX(100%)' }))
+            ])
+        ])
+    ]
 })
 
 export class ResumoInfosComponent implements OnInit {
@@ -13,14 +26,5 @@ export class ResumoInfosComponent implements OnInit {
     constructor(public holderService: HolderService) { }
 
     public ngOnInit() { }
-
-    // public hoResumo() {
-    //     if (this.tableResumoAtivo) {
-    //         this.tableResumoAtivo = false;
-    //         //this.holderService.resumoInfosAtivo = false;
-    //     } else {
-    //         this.tableResumoAtivo = true;
-    //     }
-    // }
 
 }
