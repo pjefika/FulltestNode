476b6eddbe4c4e678f8b49d8c34dbf7a2b4b55d0
FabioClem <fabio.henrique.clem@gmail.com>
2017-11-24 17:02:56 -0200

    animate speed *koalization*

--- src/template/template.component.ts
+++ src/template/template.component.ts
@@ -19,10 +19,10 @@ import { trigger, state, style, animate, transition } from '@angular/animations'
             state('in', style({ transform: 'translateX(0)' })),
             transition('void => *', [
                 style({ transform: 'translateX(100%)' }),
-                animate(100)
+                animate(300)
             ]),
             transition('* => void', [
-                animate(100, style({ transform: 'translateX(100%)' }))
+                animate(300, style({ transform: 'translateX(100%)' }))
             ])
         ])
     ]
