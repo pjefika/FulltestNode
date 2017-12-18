7ffc0e8eefeff56ab3f9f9d92d7297b7be92daca
FabioClem <fabio.henrique.clem@gmail.com>
2017-12-18 19:13:21 -0200

    evento massivo modificado... (incompleto)

--- src/util/comp_complementares/evento-massivo/evento-massivo.component.ts
+++ src/util/comp_complementares/evento-massivo/evento-massivo.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { EventoMAssivoService } from 'util/comp_complementares/evento-massivo/evento-massivo.service';
 import { CallAlertService } from 'util/callalerts/call-alert.service';
 import { ToastyComponent } from 'util/toasty/toasty.component';
-import { EventoMassivo } from 'viewmodel/evento-massivo/evento-massivo';
+import { Evento } from 'viewmodel/evento-massivo/eventos';
 
 @Component({
     selector: 'evento-massivo-component',
@@ -15,7 +15,7 @@ export class EventoMassivoComponent extends CallAlertService implements OnInit {
 
     private searching: boolean = false;
 
-    private eventoMassivo: EventoMassivo[];
+    @Input() public eventos: Evento;
 
     constructor(
         public toastyComponent: ToastyComponent) {
@@ -28,15 +28,22 @@ export class EventoMassivoComponent extends CallAlertService implements OnInit {
 
     private contagemRegressivaParaMostrarTabela() {
         this.searching = true;
+
+        this.eventos = {
+            eventos: [
+                {
+                    tipoAlarme: "INTERROMPIDO",
+                    tipoFalha: "FTTx",
+                    tipoAfetacao: "SEM AFETACAO COM RISCO",
+                    desc: "PON DOWN: PON 1/1/3/3 Dying Gasp = [0] ::PON LOSS | LOSS= 26",
+                    dataAbertura: 1513627226000,
+                    dataPrevista: 1513640984000
+                }
+            ],
+            dataConsulta: 1513630092022
+        }
+
         setTimeout(() => {
-            // this.eventoMassivo = [{
-            //     validacao: "Massivo 1",
-            //     criticidade: "Critico"
-            // },
-            // {
-            //     validacao: "Massivo 2",
-            //     criticidade: "Baixo"
-            // }];
             this.searching = false;
         }, 1000);
     }
