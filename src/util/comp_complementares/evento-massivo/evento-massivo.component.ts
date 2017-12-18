9af897a05d102d106a6b4115a97cb8ec55d226d7
FabioClem <fabio.henrique.clem@gmail.com>
2017-12-18 10:51:40 -0200

    adicionado viewmodel e mock para o evento massivo.

--- src/util/comp_complementares/evento-massivo/evento-massivo.component.ts
+++ src/util/comp_complementares/evento-massivo/evento-massivo.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { EventoMAssivoService } from 'util/comp_complementares/evento-massivo/evento-massivo.service';
 import { CallAlertService } from 'util/callalerts/call-alert.service';
 import { ToastyComponent } from 'util/toasty/toasty.component';
+import { EventoMassivo } from 'viewmodel/evento-massivo/evento-massivo';
 
 @Component({
     selector: 'evento-massivo-component',
@@ -13,6 +14,8 @@ import { ToastyComponent } from 'util/toasty/toasty.component';
 export class EventoMassivoComponent extends CallAlertService implements OnInit {
 
     private searching: boolean = false;
+
+    private eventoMassivo: EventoMassivo[];
 
     constructor(
         public toastyComponent: ToastyComponent) {
@@ -26,6 +29,14 @@ export class EventoMassivoComponent extends CallAlertService implements OnInit {
     private contagemRegressivaParaMostrarTabela() {
         this.searching = true;
         setTimeout(() => {
+            this.eventoMassivo = [{
+                validacao: "Wisetool fora...",
+                criticidade: "Critico"
+            },
+            {
+                validacao: "SAS fora....",
+                criticidade: "Baixo"
+            }];
             this.searching = false;
         }, 1000);
     }
